71a30f17b19cbab6281a2f16f4437c3b6640d35e
Damian Sygidus <damian.sygidus@stxnext.pl>
2020-10-22 18:54:56 +0200

    Init

---
+++ main.go
@@ -0,0 +1,35 @@
+package calendar_events_bot
+
+import (
+	"log"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	var (
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL")
+		token     = os.Getenv("TOKEN")
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Handle("ping", func(m *tb.Message) {
+		b.Send(m.Sender, "pong")
+	})
+}
