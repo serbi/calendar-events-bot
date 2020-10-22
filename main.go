fd1839337e3a1ef5bd2c03ec3310af79ba99d528
Damian Sygidus <damian.sygidus@stxnext.pl>
2020-10-22 23:35:01 +0200

    Replaced webhook endpoint

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	var port = os.Getenv("PORT")
 
-	err := http.ListenAndServe(":"+port+"/webhook", http.HandlerFunc(routes.WebhookHandler))
+	err := http.ListenAndServe(":"+port, http.HandlerFunc(routes.WebhookHandler))
 	if err != nil {
 		panic(err)
 	}
